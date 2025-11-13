bf821293c1ca4194b944150ebac663e8e2bd0641
W3bm@steR <undertone252passinb.undertone252@passinbox.com>
2025-11-13 12:29:29 +0200

    style: Update logo text styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       {/* LOGO */}
       <Link to="/" className="flex items-center gap-4 text-2xl font-bold">
         <Image src="logo.png" alt="Lama Logo" w={32} h={32} />
-        <span>lamalog</span>
+        <span className="capitalice">NewPress</span>
       </Link>
       {/* MOBILE MENU */}
       <div className="md:hidden">
